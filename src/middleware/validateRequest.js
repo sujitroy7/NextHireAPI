4978efc0a40bde9b7c8d77d2f9198df34daeec16
Sujit Roy <sujitroy78148@gmail.com>
2026-02-05 19:36:23 +0530

    Modify validation Middleware

--- src/middleware/validateRequest.js
+++ src/middleware/validateRequest.js
@@ -8,15 +8,21 @@ export const validateRequest = (schema) => {
         query: req.query,
         params: req.params,
       });
-
-      req.body = parsed.body;
-      // req.query = parsed.query;
-      req.params = parsed.params;
+      if (parsed.body) {
+        req.body = { ...req.body, ...parsed.body };
+      }
+      if (parsed.query) {
+        req.query = { ...req.query, ...parsed.query };
+      }
+      if (parsed.params) {
+        req.params = { ...req.params, ...parsed.params };
+      }
 
       next();
     } catch (error) {
       if (error instanceof ZodError) {
         return res.status(400).json({
+          status: "error",
           message: "Validation failed",
           errors: JSON.parse(error.message).map((err) => ({
             path: err.path.join("."),
